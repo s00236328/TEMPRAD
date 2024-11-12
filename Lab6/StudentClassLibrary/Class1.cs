3342e36db61323ed9d98733745b1c064cf438254
daniel <s00236328@atu.ie>
2024-11-12 18:15:28 +0000

    week6.1

---
+++ Lab6/StudentClassLibrary/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace StudentClassLibrary
+{
+    public class Class1
+    {
+     
+    }
+    public class Student 
+    {
+        public int Id { get; set; }
+        public int Age {  get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public ICollection<Course> Courses { get; set; }= new List<Course>();
+    }
+    public class Course
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string Lecturer {  get; set; }
+        public ICollection<Student> Students { get; set; } = new List<Student>();
+    }
+}
+

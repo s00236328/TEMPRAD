d16e89059b8099daf6f5beb354de58fb99a5a5b1
daniel <s00236328@atu.ie>
2024-11-26 23:31:19 +0000

    Start up done

--- Lab7P3/Program.cs
+++ Lab7P3/Program.cs
@@ -21,7 +21,12 @@ namespace Lab7P3
             builder.Services.AddRazorPages();
 
             var app = builder.Build();
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
 
+                SeedData.Initialize(services);
+            }
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {

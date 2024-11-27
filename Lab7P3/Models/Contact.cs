001a47095df95e39643f30aa97de53c819bc84de
daniel <s00236328@atu.ie>
2024-11-27 02:52:34 +0000

    small

--- Lab7P3/Models/Contact.cs
+++ Lab7P3/Models/Contact.cs
@@ -5,6 +5,8 @@ namespace Lab7P3.Models
     public class Contact
     {
         public int ContactId { get; set; }
+        // user ID from AspNetUser table.
+        public string? OwnerID { get; set; }
         public string? Name { get; set; }
         public string? Address { get; set; }
         public string? City { get; set; }
@@ -12,5 +14,13 @@ namespace Lab7P3.Models
         public string? Zip { get; set; }
         [DataType(DataType.EmailAddress)]
         public string? Email { get; set; }
+        public ContactStatus Status { get; set; }
+    }
+
+    public enum ContactStatus
+    {
+        Submitted,
+        Approved,
+        Rejected
     }
 }
